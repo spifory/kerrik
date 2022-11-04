c883d2ab57bd6d5d573bf9b33a5b4760001d49c9
toolifelesstocode <toolifelesstocode@gmail.com>
2022-11-04 17:22:05 +0200

    feat(main): add a try and except to ext loader & logging

--- kerrik/__main__.py
+++ kerrik/__main__.py
@@ -1,5 +1,5 @@
 import asyncio
-from logging import basicConfig, INFO
+from logging import basicConfig, INFO, getLogger
 from kerrik.bot import Kerrik
 from os import listdir
 from importlib import import_module
@@ -7,6 +7,7 @@ from importlib import import_module
 
 async def start() -> None:
     client = Kerrik()
+    log = getLogger(__name__)
     basicConfig(
         format="[%(asctime)s] | %(name)s | %(levelname)s | %(message)s",
         level=INFO,
@@ -14,7 +15,13 @@ async def start() -> None:
     )
     for e in listdir("kerrik/exts"):
         if e.endswith(".py"):
-            import_module(f"kerrik.exts.{e[:3]}").setup()
+            try:
+                import_module(f"kerrik.exts.{e[:-3]}").setup(client)
+                log.info(f"{e} has loaded")
+            except Exception as e:
+                log.error("Error while loading extensions: %s", e)
+                log.info("Aborting connection")
+                exit(1)
     await client.start()
 
 if __name__ == "__main__":

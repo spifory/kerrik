dd698762b0a5567d49a14715d359179045680ebd
toolifelesstocode <toolifelesstocode@gmail.com>
2022-11-07 18:01:47 +0200

    refactor(bot): use disnake stuff

--- kerrik/bot.py
+++ kerrik/bot.py
@@ -1,16 +1,38 @@
 import datetime
 from logging import getLogger
-from pydris.client import Client
+from disnake import Activity, ActivityType, Intents
+from disnake.ext.commands import Bot, CommandSyncFlags
+import os
+from dotenv import load_dotenv
 
 LOG = getLogger(__name__)
+load_dotenv()
 
 
-class Kerrik(Client):
+class Kerrik(Bot):
     def __init__(self):
         self.start_time: datetime.datetime
-        super().__init__("Kerrik", prefix="-")
+        self.config = os.environ
+        super().__init__(
+            ["-", "kerrik"],
+            help_command=None,  # type: ignore
+            case_insensitive=True,
+            intents=Intents(
+                message_content=True,
+                guild_messages=True,
+                members=True
+            ),
+            command_sync_flags=CommandSyncFlags(
+                allow_command_deletion=True,
+                sync_commands=True
+            ),
+            activity=Activity(
+                name="in Elupiss",
+                type=ActivityType.playing
+            )
+        )
 
-    async def start(self) -> None:
+    async def start(self, token: str) -> None:
         self.start_time = datetime.datetime.now()
         LOG.info("Starting Kerrik...")
-        return await super().start()
+        return await super().start(token)

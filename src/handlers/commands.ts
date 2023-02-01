fb4db48547e8f5ea4ff0e281bcca8d8d64ce3c42
toolifelesstocode <toolifelesstocode@gmail.com>
2023-02-01 11:39:57 +0200

    refactor: use TypeScript (#5)
    * chore(LICENSE): add `present` keyword
    
    * chore(gitignore): ignore ts-related contents
    
    * refactor: remove python files
    
    * build: setup package config
    
    * ci(dependabot): change package-ecosystem to npm
    
    * ci(dependabot): change update time to weekly
    
    Because npm 💀
    
    * chore: add TS configuration file
    
    * feat: add bot subclass
    
    * feat: add main file and command handler
    
    * feat: add file handlers
    
    * refactor: add cmds/events using new handler
    
    * feat: add base stuff

---
+++ src/handlers/commands.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath } from "url";
+import { readdirSync } from "fs";
+import path from 'path';
+import { Message } from "evangeline";
+import { Kerrik } from "../bot.js";
+
+export default interface Command {
+    name: string;
+    description: string;
+    aliases?: string[];
+    callback: (bot: Kerrik, msg: Message, args: string[]) => void;
+}
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export const loadCommands = (bot: Kerrik) => {
+    readdirSync(path.join(__dirname, '..', 'commands')).forEach(async (file) => {
+        const { command } = await import(path.join(__dirname, '..', 'commands', file));
+        bot.addCommand(command.name, command.aliases || [], command.callback);
+    });
+};

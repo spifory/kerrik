322226e9d1dd8b748cb8de18d056b8dd47596216
Sham <thesham@protonmail.com>
2023-02-01 12:24:30 +0200

    refactor: add underscores to unused params

--- src/commands/upSince.ts
+++ src/commands/upSince.ts
@@ -4,12 +4,8 @@ export const command: Command = {
     name: 'upsince',
     description: 'See how long I\'ve been online',
     aliases: ['uptime', 'up-since'],
-    callback: (bot, msg, args) => {
-        // create a stringUpTime constant that will return in the following format: YYYY-MM-DD HH:MM:SS E.g. 2023-02-01 23:59:39
+    callback: (bot, _msg, _args) => {
         const stringUpTime = new Date(bot.startTime).toLocaleString('en-ZA')
-        // create a upTime constant that will return in the following format: HH:MM:SS E.g. 23:59:39
-        const upTime = Math.round(bot.startTime / 1000)
-        // send a message to the channel that the command was sent in
-        bot.sendMessage(`I've been online since ${stringUpTime} <t:${upTime}:R>`)
+        bot.sendMessage(`I've been online since ${stringUpTime}`)
     }
 }
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,7 @@ import Event from '../handlers/events'
 
 export const event: Event<'ready'> = {
     name: 'ready',
-    callback: (bot) => {
+    callback: (_bot) => {
         console.info(`[I] Kerrik is ready!`)
     }
 }
